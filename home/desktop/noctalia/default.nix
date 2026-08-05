f0b50569127c9165617c0b483aa6f4b7891335ad
alexy-despres <alexydespres.dev@gmail.com>
2026-08-05 10:56:46 -0400

    change: noctalia settings

--- home/desktop/noctalia/default.nix
+++ home/desktop/noctalia/default.nix
@@ -19,10 +19,7 @@
   home.packages = with pkgs; [
     inputs.gslapper.packages.${pkgs.system}.gslapper
     gst_all_1.gstreamer
-    # gst_all_1.gst-plugins-base
-    # gst_all_1.gst-plugins-good
-    # gst_all_1.gst-plugins-bad
-    # gst_all_1.gst-plugins-ugly
     socat
+    qrencode
   ];
 }
